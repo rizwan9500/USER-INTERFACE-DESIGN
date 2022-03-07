8bb08792cb4ae60e0f5926695c249eb0d5f9205d
rizwa <rizwanahmed>
2022-03-08 00:57:47 +0530

    master

--- Cognizant-Early-Engagement-main/Java Programming Funcamentals/Highest Placement/Placement.java
+++ Cognizant-Early-Engagement-main/Java Programming Funcamentals/Highest Placement/Placement.java
@@ -1,30 +1,28 @@
 import java.util.*;
 import java.util.stream.*;
-
-
 class Placement {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int cse, ece, mech;
-        
+
         System.out.println("Enter the no of students placed in CSE:");
         cse = scanner.nextInt();
         System.out.println("Enter the no of students placed in ECE:");
         ece = scanner.nextInt();
         System.out.println("Enter the no of students placed in MECH");
         mech = scanner.nextInt();
-        
+
         List<Integer> list = new TreeSet<>(Arrays.asList(cse, ece, mech)).stream().collect(Collectors.toList());
-        
+
         if (list.get(0) < 0) {
             System.out.println("Input is Invalid");
         } else if (list.size() == 1) {
             System.out.println("None of the department has got the highest placement");
         } else {
             System.out.println("Highest placement");
-            
+
             int highest = list.get(list.size() - 1);
-            
+
             if (list.size() == 2) {
                 if (cse == highest && ece == highest) {
                     System.out.println("CSE");
